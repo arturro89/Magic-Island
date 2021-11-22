7817478a0de86abd371f297e3206c6bf12780c0d
Artur Hangiel <artur.hangiel@gmail.com>
2021-11-22 14:07:02 +0100

    Script.js + active menu added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Navigation menu - responsive
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
